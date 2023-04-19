f3174502195aae560af8bf7ebf550a3f11889f39
MounaGL <mounikaglmouna@gmail.com>
2023-04-19 17:17:01 +0530

    c code

---
+++ c/addnum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Enter two integers:");
+    scanf("%d %d", &a, &b);
+
+    c = a+b;
+    printf("Sum of %d and %d is %d", a, b, c);
+    return 0;
+}
