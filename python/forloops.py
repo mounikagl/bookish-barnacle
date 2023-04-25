2100047b604b9cfe048b3cc38198588c44bc50a1
MounaGL <mounikaglmouna@gmail.com>
2023-04-25 10:57:17 +0530

    New code Added

---
+++ python/forloops.py
@@ -0,0 +1,20 @@
+# my_list = [1,2,3,4,5,6,7,8,9,10]
+
+# for num in my_list:
+#     print(f" 3 * {num} = ", num*3)
+
+# word  = "Hello World"
+
+# for i in word:
+#     print(i)
+
+# dict = {'num1':1, 'num2':2, 'num3':3}
+
+# for key in dict.keys():
+#     print(key)
+
+# list = [(1,2),(2,4),(5,6),(7,8)]
+
+# for a,b in list:
+#     print("a is :", a)
+#     print("b is :", b)
---
+++ python/whileloops.py
@@ -0,0 +1,26 @@
+# a =0 
+
+# while a<5:
+#     print(a)
+#     a = a+1
+
+***break***
+# x= [0,1,2,3,4,5,6,7]
+
+# for n in x:
+#     # print(n)
+#     if n>5:
+#         break;
+#     print(n)
+***continue***
+# word = "mounika"
+
+# for letter in word:
+#     if letter == 'k':
+#         continue;
+#     print(letter)
+***pass***
+# for letter in word:
+#     pass;
+#     print(letter)
+# print("Word displayed")
